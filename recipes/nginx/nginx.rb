8a718a87ffb04a99197347073fcb2d9ed226f8b8
sawada <sawadasuiren@gmail.com>
2017-02-10 20:09:27 +0900

    Add nginx.conf template.

--- recipes/nginx/nginx.rb
+++ recipes/nginx/nginx.rb
@@ -1,1 +1,15 @@
 package 'nginx'
+
+file "/etc/nginx/conf.d/default.conf" do
+  action :delete
+  notifies :reload, "service[nginx]"
+end
+
+template '/etc/nginx/nginx.conf' do
+  source './templates/etc/nginx/nginx.conf'
+  notifies :reload, "service[nginx]"
+end
+
+service 'nginx' do
+  action :start
+end
